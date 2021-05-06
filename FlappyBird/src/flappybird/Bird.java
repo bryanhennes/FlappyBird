920ff59d8bf38b7126b1c58e42441753318fe33e
bryanhennes <bryanhennes@localhost>
2021-05-05 20:13:56 -0400

    added high score tracker edited title screen

--- FlappyBird/src/flappybird/Bird.java
+++ FlappyBird/src/flappybird/Bird.java
@@ -6,7 +6,6 @@
 package flappybird;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
@@ -15,23 +14,19 @@ import javafx.scene.shape.Circle;
  * @author bryanhennes
  */
 public class Bird extends Circle {
-    private ImageView imageView;
     private Image image;
     private ImagePattern ip;
-    private int velocity;
     private Circle bird;
     
     public Bird(int centerX, int centerY, int radius){
       
         this.bird = new Circle();
-        this.imageView = new ImageView();
         this.image = new Image(getClass().getResourceAsStream("flappy_bird.png"));
         this.ip = new ImagePattern(this.image);
         this.bird.setCenterX(centerX);
         this.bird.setCenterY(centerY);
         this.bird.setRadius(radius);
         this.bird.setFill(this.ip);
-        this.velocity = 50;
     }
     
     
--- FlappyBird/src/flappybird/FlappyBird.java
+++ FlappyBird/src/flappybird/FlappyBird.java
@@ -5,6 +5,7 @@
  */
 package flappybird;
 
+import java.io.File;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -23,9 +24,11 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -58,6 +61,19 @@ public class FlappyBird extends Application {
     Image pressEnter;
     Label scoreCounter;
     String counter;
+    Image grass;
+    ImagePattern grassIp;
+    Rectangle ground;
+    ImageView gameOverImageView;
+    Image gameOverImg;
+    Label highScoreTracker;
+    String highScoreStr;
+    int highScore;
+    int previousScore;
+    Text restart;
+    ImageView titleImageView;
+    Image titleImage;
+   
     
     void addPipe(){
         int space =400;
@@ -74,13 +90,53 @@ public class FlappyBird extends Application {
      void hitsPipe(){
          
            for(int i =0; i < pipes.size(); i++){ 
-            if(bird.getBoundsInParent().intersects(pipes.get(i).getBoundsInParent())){
-                  gameOver = true;       
+            if(bird.getBoundsInParent().intersects(pipes.get(i).getBoundsInParent()) || bird.getCenterY() > 770 || bird.getCenterY() < 50){
+                  gameOver = true;
+                  
             }
             
            }
+           
+           if(gameOver){
+               tl.pause();
+           }
              
      
+     }
+     
+     
+     //run this method if game is not being launched for the first time
+     void startNewGame(){
+         flappyBird.fall();
+         bird.setCenterX(50);
+         bird.setCenterY(700);
+         gameOver = false;
+         tl.pause();
+         highScore = previousScore;
+         
+         //check to see if current score was higher than prevous highscore
+         if(score > highScore){
+             highScore = score;
+             previousScore = highScore;
+         }
+         highScoreStr = String.valueOf(highScore);
+         highScoreTracker.setText("Best Score : " + highScoreStr); //display current highscore
+         root.getChildren().add(highScoreTracker);
+         
+         
+         
+         scene.setOnKeyReleased(e -> {
+                if(e.getCode() == KeyCode.ENTER){
+                    startGame();
+                    root.getChildren().remove(gameOverImageView);
+                    root.getChildren().remove(highScoreTracker);
+                    root.getChildren().remove(scoreCounter);
+                    root.getChildren().remove(restart);
+                    root.getChildren().add(pressEnterImageView);
+                    root.getChildren().add(titleImageView);
+                }
+            });
+         
      }
      
   
@@ -90,10 +146,13 @@ public class FlappyBird extends Application {
          bird.setCenterY(700);
          gameOver = false;
          score = 0;
+         highScore = 0;
          counter = String.valueOf(score);
          scoreCounter.setText("Score: " + counter);
          root.getChildren().removeAll(pipes);
          pipes.clear();
+         
+         
          
          int i = 0;
          while (i< 200){
@@ -109,10 +168,15 @@ public class FlappyBird extends Application {
             scene.setOnKeyReleased(e -> {
                 if(e.getCode() == KeyCode.ENTER){
                     root.getChildren().addAll(pipes);
+                    root.getChildren().add(scoreCounter);
                     tl.play();
+                    bird.setCenterY(400);
+                    
                     root.getChildren().remove(pressEnterImageView);
+                    root.getChildren().remove(titleImageView);
                 }
             });
+            
             
     
      }   
@@ -129,10 +193,18 @@ public class FlappyBird extends Application {
         //set 'Press Enter to Play' image
         pressEnter = new Image(getClass().getResourceAsStream("pressEnter.png"));
         pressEnterImageView = new ImageView(pressEnter);
-        pressEnterImageView.setFitHeight(300);
-        pressEnterImageView.setFitWidth(400);
-        pressEnterImageView.setX(stageWidth/2-200);
-        pressEnterImageView.setY(50);
+        pressEnterImageView.setFitHeight(200);
+        pressEnterImageView.setFitWidth(300);
+        pressEnterImageView.setX(stageWidth/2-150);
+        pressEnterImageView.setY(200);
+        
+        //set game over image on screen when losing
+        gameOverImg = new Image(getClass().getResourceAsStream("gameover.png"));
+        gameOverImageView = new ImageView(gameOverImg);
+        gameOverImageView.setFitHeight(300);
+        gameOverImageView.setFitWidth(400);
+        gameOverImageView.setX(stageWidth/2-200);
+        gameOverImageView.setY(50);
         
         
         //create score label at the top to keep track of current score
@@ -145,9 +217,48 @@ public class FlappyBird extends Application {
         scoreCounter.setPrefWidth(300);
         scoreCounter.setFont(font);
         
+        //label to keep track of high score
+        highScoreTracker = new Label();
+        highScoreStr = String.valueOf(highScore);
+        highScoreTracker.setText("Score: " + highScoreStr);
+        Font font2 = Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 30);
+        highScoreTracker.setAlignment(Pos.CENTER);
+        highScoreTracker.setPrefHeight(100);
+        highScoreTracker.setPrefWidth(300);
+        highScoreTracker.setFont(font2);
+        highScoreTracker.setLayoutY(100);
+        
+        
+        restart = new Text();
+        restart.setText("Press Enter to Play Again!");
+        restart.setX(300);
+        restart.setY(30);
+        Font font3 = Font.font(40);
+        restart.setFont(font3);
         
       
-       
+        
+        titleImage = new Image(getClass().getResourceAsStream("flappy-bird-logo-png-transparent.png"));
+        titleImageView = new ImageView(titleImage);
+        titleImageView.setFitHeight(100);
+        titleImageView.setFitWidth(500);
+        titleImageView.setX(250);
+        titleImageView.setY(70);
+        
+        
+        
+        //create ground
+        grass = new Image(getClass().getResourceAsStream("grass.png"));
+        grassIp = new ImagePattern(grass);
+        ground = new Rectangle();
+        ground.setX(0);
+        ground.setY(700);
+        ground.setWidth(1000);
+        ground.setHeight(100);
+        ground.setFill(grassIp);
+        
+     
+        //pipes arrayList
         pipes = new ArrayList<Rectangle>();
         
         
@@ -155,9 +266,7 @@ public class FlappyBird extends Application {
     
         //this keyframe runs every 40 milliseconds..checks bird position every 40 milliseconds
         KeyFrame trackBird = new KeyFrame(Duration.millis(20), (ActionEvent event) -> {
-                    System.out.println(bird.getCenterY());
-                    System.out.println(score);
-                    
+
                    for(int i = 0; i < pipes.size(); i++){
                        Rectangle pipe = pipes.get(i);
                        pipe.setFill(Color.GREEN);
@@ -191,20 +300,26 @@ public class FlappyBird extends Application {
                     
                     hitsPipe(); //check for bird hitting pipe
                     
-                    //check for collision
+                    
                     if(gameOver){
-                        root.getChildren().add(pressEnterImageView); //add 'Press Enter to Start' back if game has finished
-                        startGame(); //if game is over restart original start() method
+                        root.getChildren().add(gameOverImageView);//add game over image to the screen when losing
+                        root.getChildren().add(restart); //add 'Press Enter to Play Again' when losing
+                        startNewGame(); //if game ends, run startNewGame method                            
                     }
                     
                 });
         
         
         
-        root = new Group();     
+        root = new Group();  
+        
+        root.getChildren().add(ground);
         root.getChildren().add(bird); //add bird object to root
         root.getChildren().add(pressEnterImageView); //add 'Press Enter to Play' to screen
-        root.getChildren().add(scoreCounter); //add score counter text at top of screen
+        root.getChildren().add(titleImageView);
+
+        
+        
         scene = new Scene(root, stageHeight, stageWidth, Color.SKYBLUE);
         stage.setScene(scene);
         
